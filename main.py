7069a4ce229daddc340bfca550446d4c7b9d5d03
g1ebi7 <glebviltov@gmail.com>
2024-11-18 17:36:45 +0200

    initial

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print('Hollllllllllaaaaaaa')
+
+# https://github.com/g1ebi7/homework.git
+
+# git remote add origin https://github.com/g1ebi7/homework.git
+# git branch -M main
+# git push -u origin main
